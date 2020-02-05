e7b3011c06ac75f9e9926d0abfecdbc28cc96810
ElMonstro <jratcher@gmail.com>
2020-02-05 12:32:44 +0300

    bug(typo): fix settings typo

--- jamboSms/settings.py
+++ jamboSms/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "drf_yasg",
-    "corsheaders"
+    "corsheaders",
     "core",
     "api.authentication",
     "api.sms"
